39289a02249f2c927de38993b349adc55887f4ef
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-08-22 14:15:58 +0200

    back

--- marcoprank/app/api/marco/route.js
+++ marcoprank/app/api/marco/route.js
@@ -8,7 +8,7 @@ export async function GET(request) {
         }
 
         const html = await response.text();
-        arg = html.split('<div class="title">\n                <a                    href="')[1]
+        let arg = html.split('<div class="title">\n                <a                    href="')[1]
         arg = arg.split(`" onclick="setEntryCookie('VidPg-premVid')"   class="">`)[0]
         let url = "https://fr.pornhub.com" + arg
 
